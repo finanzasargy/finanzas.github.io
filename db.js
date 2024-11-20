934e401e50e4a370a07ad5014bc922aa0a7efb66
Dezz <79386394+finanzasargy@users.noreply.github.com>
2024-11-20 18:40:11 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,26 @@
+{
+  "x-api-key": "64d57b57a86d3227b097c31e",
+  "Content-Type": "application/json"
+},
+{
+  "id": 1,
+  "name": "Dólar",
+  "sell": "30.00",
+  "buy": "29.50",
+  "change": "+1.00%",
+  "trend": "up"
+},
+{
+  "id": 2,
+  "message": "Moneda actualizada",
+  "currency": {
+    "name": "Dólar",
+    "sell": "30.00",
+    "buy": "29.50",
+    "change": "+1.00%",
+    "trend": "up",
+    "time": "20/11/2024, 15:35:00"
+  }
+}
+
+
